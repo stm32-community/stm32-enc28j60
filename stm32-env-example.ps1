7b7648c17c7b1c3ed471b7e39fa4e5676d96d3c1
mykola2312 <nikolay_tihonov@i.ua>
2024-01-07 21:31:06 +0200

    begin porting to CMake using stm32-base

---
+++ stm32-env-example.ps1
@@ -0,0 +1,3 @@
+$env:STM32_TOOLCHAIN_PATH = "<your path to stm32-cmake>\cmake\stm32_gcc.cmake"
+$env:STM32_TARGET_TRIPLET = "arm-none-eabi"
+$env:STM32_CUBE_F4_PATH="<path to stm32 cube firmware drives>"
