e52c6601650f619ff86bc65602b26d72da2219d7
DtNeo <a.denot@aximedfrance.com>
2024-06-06 12:15:06 +0200

    2 files for RTC

---
+++ inc/RTC.h
@@ -0,0 +1,25 @@
+/*
+ * RTC.h
+ *
+ *  Created on: Jun 3, 2024
+ *      Author: dtneo
+ */
+
+#ifndef INC_RTC_H_
+#define INC_RTC_H_
+
+#include "main.h"
+#include "defines.h"
+
+typedef struct {
+    uint8_t day;
+    uint8_t month;
+    uint16_t year;
+    uint8_t hours;
+    uint8_t minutes;
+    uint8_t seconds;
+} DateTime;
+
+DateTime getRTCDateTime();
+
+#endif /* INC_RTC_H_ */
